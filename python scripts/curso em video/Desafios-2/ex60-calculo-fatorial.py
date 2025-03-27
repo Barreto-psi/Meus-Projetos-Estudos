835678a738b371b3e18daaf04e81464ee02ef5a5
Barreto-psi <gabriel.haran@gmail.com>
2025-03-26 20:25:38 -0400

    ex060

---
+++ python scripts/curso em video/Desafios-2/ex60-calculo-fatorial.py
@@ -0,0 +1,19 @@
+n = int(input('Digite um numero para calcular seu fatorial:'))
+c = n
+f = 1
+print('Calculando {}! = '.format(n),end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' X ' if c > 1 else ' = ',end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
+#outra forma
+'''
+from math import factorial
+n = int(input('Fatorial de : '))
+f = factorial(n)
+print('O fatorial de {} e {}'.format(n,f))
+
+'''
